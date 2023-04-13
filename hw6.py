f55f407683bb87d027e0d9ae34db4a762a1da799
hakyeop <127179902+hakyeop@users.noreply.github.com>
2023-04-13 14:45:11 +0900

    7주차 과제
    7주차 과제

---
+++ hw6.py
@@ -0,0 +1,13 @@
+print()
+for i in range(1, 10, 1):
+    for o in range(2, 6, 1):
+        print(f'{o} x {i} = {i*o:2d}', end='     ')
+    print()
+
+print()
+
+for i in range(1, 10, 1):
+    for o in range(6, 10, 1):
+        print(f'{o} x {i} = {i*o:2d}', end='     ')
+    print()
+print()
